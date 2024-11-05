0e400f7f134a5af59da8e69313660e0ff8603454
박지환 <bat522@naver.com>
2024-11-05 09:45:05 +0900

    Notification api db와 연결

---
+++ datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/data/models/Notification.kt
@@ -0,0 +1,11 @@
+package com.google.android.horologist.datalayer.sample.screens.hotdog.data.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Notification(
+    @SerialName("user_id") val userId: Long,
+    @SerialName("type_id") val typeId: Int,
+    @SerialName("content") val content: String = ""
+)
--- datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/notification/NotificationScreen.kt
+++ datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/notification/NotificationScreen.kt
@@ -4,29 +4,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import com.google.android.horologist.datalayer.sample.screens.hotdog.common.ButtonFooter
 import com.google.android.horologist.datalayer.sample.screens.hotdog.notification.components.NotificationPage
+import com.google.android.horologist.datalayer.sample.screens.hotdog.repository.NotificationRepository
 
 @Composable
 fun NotificationScreen(navController: NavHostController) {
-    val notifications = listOf(
-        "밥 시간이야! 밥 줘!",
-        "자네 남자친구가 이모티콘을 보냈다네..",
-        "산책하지 않을래?",
-        "밥 시간이야! 밥 줘!",
-        "자네 남자친구가 이모티콘을 보냈다네..",
-        "산책하지 않을래?",
-        "밥 시간이야! 밥 줘!",
-        "자네 남자친구가 이모티콘을 보냈다네..",
-        "산책하지 않을래?",
-        "밥 시간이야! 밥 줘!",
-        "자네 남자친구가 이모티콘을 보냈다네..",
-        "산책하지 않을래?"
-    )
+    val notificationRepository = NotificationRepository()
+    var notifications by remember { mutableStateOf(listOf<String>()) }
+
+    LaunchedEffect(Unit) {
+        val fetchedNotifications = notificationRepository.getNotificationsByUserId(1) // userID가 1인 사용자의 알림을 가져옴
+        val typeIds = fetchedNotifications.map { it.typeId }
+        val notificationTypes = notificationRepository.getNotificationContentByTypeIds(typeIds)
+
+        // category만 포함하도록 수정
+        notifications = fetchedNotifications.map { notification ->
+            notificationTypes.find { it.typeId == notification.typeId }?.category ?: "Unknown"
+        }
+    }
 
     Box(
         modifier = Modifier
