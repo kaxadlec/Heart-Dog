cfed59d78f6fbffa502db9e1712c2e7b07f521e8
박지환 <bat522@naver.com>
2024-11-05 10:38:47 +0900

    MVVM 패턴으로 변경하기 위해 modelview 추가

--- datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/notification/NotificationScreenViewModel.kt
+++ datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/notification/NotificationScreenViewModel.kt
@@ -1,4 +1,30 @@
 package com.google.android.horologist.datalayer.sample.screens.hotdog.notification
 
-class NotificationViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.google.android.horologist.datalayer.sample.screens.hotdog.repository.NotificationRepository
+
+class NotificationViewModel : ViewModel() {
+    private val notificationRepository = NotificationRepository()
+    private val _notifications = MutableStateFlow<List<String>>(emptyList())
+    val notifications: StateFlow<List<String>> = _notifications
+
+    init {
+        fetchNotifications()
+    }
+
+    private fun fetchNotifications() {
+        viewModelScope.launch {
+            val fetchedNotifications = notificationRepository.getNotificationsByUserId(1)
+            val typeIds = fetchedNotifications.map { it.typeId }
+            val notificationTypes = notificationRepository.getNotificationContentByTypeIds(typeIds)
+
+            _notifications.value = fetchedNotifications.map { notification ->
+                notificationTypes.find { it.typeId == notification.typeId }?.category ?: "Unknown"
+            }
+        }
+    }
 }
