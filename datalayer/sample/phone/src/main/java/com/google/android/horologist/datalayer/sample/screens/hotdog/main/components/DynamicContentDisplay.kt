27d79088132f475bb429f377274f8956bacb4fe7
박지환 <bat522@naver.com>
2024-11-17 23:56:31 +0900

    datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer

--- datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/main/components/DynamicContentDisplay.kt
+++ datalayer/sample/phone/src/main/java/com/google/android/horologist/datalayer/sample/screens/hotdog/main/components/DynamicContentDisplay.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.horologist.datalayer.sample.screens.hotdog.main.utils.ContentType
 
 @Composable
